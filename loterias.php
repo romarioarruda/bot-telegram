36bd4e7cf74e9f948878bbd07b07639deab6f290
Romário Arruda <romarioarruda98@gmail.com>
2020-12-07 18:38:40 -0300

    Refatorando código

--- loterias.php
+++ loterias.php
@@ -15,14 +15,7 @@ function processMessage($message) {
     $text = $message['text'];
 
     if (strpos($text, "/start") === 0) {
-        sendMessage(
-            "sendMessage", array('chat_id' => $chat_id, "text" => 'Olá, '. $message['from']['first_name'].
-        '! Eu sou um bot que informa o resultado do último sorteio da loteria caixa. Será que você ganhou dessa vez? Para começar, escolha qual sorteio você deseja ver o resultado',
-        'reply_markup' =>
-            array('keyboard' => 
-                array(array('Mega-Sena', 'Quina'), array('Lotofácil', 'Lotomania')),
-        'one_time_keyboard' => true))
-        );
+        onTimeKeyBoard($chat_id, $message['from']['first_name']);
     } else if ($text === "Mega-Sena") {
         sendMessage("sendMessage", array('chat_id' => $chat_id, "text" => $parser->getResult(New ParseMegaSena, $text)));
     } else if ($text === "Quina") {
@@ -32,22 +25,7 @@ function processMessage($message) {
     } else if ($text === "Lotofácil") {
         sendMessage("sendMessage", array('chat_id' => $chat_id, "text" => $parser->getResult(new ParseLotoFacil, $text)));
     } else if ($text === "g1") {
-        sendMessage("sendMessage", array('chat_id' => $chat_id, "text" => 'Olá, '. $message['from']['first_name'].
-		'! Escolha um dos jogos abaixo para ver o resultado diretamente no site do G1.', 
-        'reply_markup' => 
-        array('inline_keyboard' => 
-            array(
-                array(
-                    array('text'=>'Mega-Sena','url'=>'http://g1.globo.com/loterias/megasena.html'), //botão 1
-                    array('text'=>'Quina','url'=>'http://g1.globo.com/loterias/quina.html')//botão 2
-                ),
-
-                array(
-                    array('text'=>'Lotofácil','url'=>'http://g1.globo.com/loterias/lotofacil.html'), //botão 3
-                    array('text'=>'Lotomania','url'=>'http://g1.globo.com/loterias/lotomania.html')//botão 4
-                )
-            )
-        )));
+        inlineKeyboardG1($chat_id, $message['from']['first_name']);
     } else {
         sendMessage("sendMessage", array('chat_id' => $chat_id, "text" => 'Desculpe, mas não entendi essa mensagem. :('));
     }
@@ -67,7 +45,40 @@ function sendMessage($method, $parameters) {
     );
 
     $context = stream_context_create($options);
+
     file_get_contents(API_URL.$method, false, $context);
+}
+
+function onTimeKeyBoard($chat_id, $from) {
+    sendMessage(
+        "sendMessage", array('chat_id' => $chat_id, "text" => 'Olá, '. $from.
+    '! Eu sou um bot que informa o resultado do último sorteio da loteria caixa. Será que você ganhou dessa vez? Para começar, escolha qual sorteio você deseja ver o resultado',
+    'reply_markup' =>
+        array('keyboard' => 
+            array(array('Mega-Sena', 'Quina'), array('Lotofácil', 'Lotomania')),
+            'one_time_keyboard' => true
+        ))
+    );
+}
+
+function inlineKeyboardG1($chat_id, $from) {
+    sendMessage("sendMessage", array('chat_id' => $chat_id, "text" => 'Olá, '. $from.
+		'! Escolha um dos jogos abaixo para ver o resultado diretamente no site do G1.', 
+        'reply_markup' => 
+        array('inline_keyboard' => 
+            array(
+                array(
+                    array('text'=>'Mega-Sena','url'=>'http://g1.globo.com/loterias/megasena.html'), //botão 1
+                    array('text'=>'Quina','url'=>'http://g1.globo.com/loterias/quina.html')//botão 2
+                ),
+
+                array(
+                    array('text'=>'Lotofácil','url'=>'http://g1.globo.com/loterias/lotofacil.html'), //botão 3
+                    array('text'=>'Lotomania','url'=>'http://g1.globo.com/loterias/lotomania.html')//botão 4
+                )
+            )
+        ))
+    );
 }
 
 $update_response = file_get_contents("php://input");
